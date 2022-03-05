73cd0be3fea1c5c40d6d68b29e94155db0b914d7
Torrin <TPataki1234@gmail.com>
2022-03-04 16:00:30 -0800

    Added implementation to check data more accurately

--- src/main/model/Purchase.java
+++ src/main/model/Purchase.java
@@ -3,6 +3,8 @@ package model;
 import org.json.JSONObject;
 import persistence.Writable;
 
+import java.util.Objects;
+
 //Represents a history of stock purchases with the number of shares purchased and price purchased at
 public class Purchase implements Writable {
 
@@ -25,6 +27,23 @@ public class Purchase implements Writable {
     //EFFECTS: returns the total amount (in dollars) purchased in this purchase
     public Double totalInvested() {
         return (numShares * pricePurchased);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Purchase purchase = (Purchase) o;
+        return numShares == purchase.numShares && pricePurchased.equals(purchase.pricePurchased);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(numShares, pricePurchased);
     }
 
     @Override
--- src/test/persistance/JsonTest.java
+++ src/test/persistance/JsonTest.java
@@ -8,10 +8,11 @@ import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 // Code based on code found in demo project: JsonSerializationDemo
+// Represents common tests done in other Json test classes
 public class JsonTest {
     protected void checkStock(String ticker, ArrayList<Double> prices, ArrayList<Purchase> purchases, Stock stock) {
         assertEquals(ticker, stock.getTicker());
-        assertEquals(prices.size(), stock.getPriceHistory().size());
-        assertEquals(purchases.size(), stock.getPurchaseHistory().size());
+        assertEquals(prices, stock.getPriceHistory());
+        assertEquals(purchases, stock.getPurchaseHistory());
     }
 }
